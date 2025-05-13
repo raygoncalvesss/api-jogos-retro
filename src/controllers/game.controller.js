e3bec8971fa984a501cff87ae5e7883fb19a5996
rayssa <rayssagoncalves.tds2.senai@gmail.com>
2025-05-13 11:23:53 -0300

    feat: create games method

--- src/controllers/game.controller.js
+++ src/controllers/game.controller.js
@@ -11,6 +11,32 @@ class GameController {
             return res.status(500).json({ message: "Error finding all games", error });
         }
     }
+
+    async create (req, res) {
+        try {
+            const {name, platform} = req.body;
+
+            //validação basica
+            if(!name || !platform) {
+                return res.status(400).json({ message: "Name and plataform are required!" });
+            }
+
+            const data = {
+                name,
+                plataform: platform,
+            };
+
+            const newGame = await GameModel.create(data);
+
+            return res.status(201).json({
+                message: "New game successfully created!",
+                newGame,
+            })
+        }catch (error) {
+            console.error("Error creating new game", error);
+             res.status(500).json({ error: "Error creating new game" });
+        }
+    }
 }
 
 export default new GameController();
--- src/routes/game.routes.js
+++ src/routes/game.routes.js
@@ -4,6 +4,7 @@ import GameController from '../controllers/game.controller.js';
 
 const gameRouter = express.Router();
 
+gameRouter.post("/", GameController.create);
 gameRouter.get("/", GameController.findAll);
 
 export default gameRouter;
